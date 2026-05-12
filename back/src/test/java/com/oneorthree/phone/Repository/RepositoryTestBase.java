7f2043b2c3ac029610560771f728cc61a8142f95
조재영 <whwodud0303@naver.com>
2026-05-12 13:18:01 +0900

    test: repository base class 생성 - testcontainer를 사용한 테스트 환경 구성

---
+++ back/src/test/java/com/oneorthree/phone/Repository/RepositoryTestBase.java
@@ -0,0 +1,29 @@
+package com.oneorthree.phone.Repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest
+@Transactional
+public abstract class RepositoryTestBase {
+
+    static final PostgreSQLContainer<?> postgres;
+
+    static {
+        postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+                .withDatabaseName("test_db")
+                .withUsername("test")
+                .withPassword("test");
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
