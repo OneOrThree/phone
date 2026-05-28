11097d8ab1cb72e43d1f08d7c43a7000bee64640
조재영 <whwodud0303@naver.com>
2026-05-28 23:55:44 +0900

    feat: add refreshToken to User, fix SocialAccount table name typo, add findByRefreshToken

--- back/src/main/java/com/oneorthree/phone/repository/UserRepository.java
+++ back/src/main/java/com/oneorthree/phone/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // 소셜 로그인
     Optional<User> findByNickname(String nickname);
+    Optional<User> findByRefreshToken(String refreshToken);
 }
