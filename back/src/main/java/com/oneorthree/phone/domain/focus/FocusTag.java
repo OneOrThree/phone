9a585394e7d8b74009ab76780738cab41c6f64c6
조재영 <whwodud0303@naver.com>
2026-06-05 15:37:51 +0900

    GROMO-232 FIX: TAG 삭제시 포커스 세션 삭제 cascade

--- back/src/main/java/com/oneorthree/phone/domain/focus/FocusTag.java
+++ back/src/main/java/com/oneorthree/phone/domain/focus/FocusTag.java
@@ -2,20 +2,15 @@ package com.oneorthree.phone.domain.focus;
 
 import com.oneorthree.phone.domain.user.User;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "focus_tags")
@@ -35,6 +30,9 @@ public class FocusTag {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "focusTag", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<FocusSession> focusSessions = new ArrayList<>();
 
     public void updateName(String name) {
         this.name = name;
