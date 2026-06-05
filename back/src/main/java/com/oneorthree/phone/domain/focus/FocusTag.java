c565b6a67c10885198c1012316c499cd9acadb29
조재영 <whwodud0303@naver.com>
2026-06-05 15:44:49 +0900

    REFACTOR: check style 수정

--- back/src/main/java/com/oneorthree/phone/domain/focus/FocusTag.java
+++ back/src/main/java/com/oneorthree/phone/domain/focus/FocusTag.java
@@ -2,7 +2,17 @@ package com.oneorthree.phone.domain.focus;
 
 import com.oneorthree.phone.domain.user.User;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +41,7 @@ public class FocusTag {
     @Column(nullable = false)
     private String name;
 
+    @Builder.Default
     @OneToMany(mappedBy = "focusTag", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<FocusSession> focusSessions = new ArrayList<>();
 
