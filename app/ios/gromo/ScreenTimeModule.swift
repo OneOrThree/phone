9129632779dc4e009c652029d0f7234863de59ed
Ahn <ahnsoobin6870@gmail.com>
2026-06-14 14:35:49 +0900

    feat: extension 배포 target재설정

--- app/ios/gromo/ScreenTimeModule.swift
+++ app/ios/gromo/ScreenTimeModule.swift
@@ -86,6 +86,8 @@ class ScreenTimeModule: NSObject {
     ) {
         let sharedDefaults = UserDefaults(suiteName: "group.com.oneorthree.gromo")
         let totalDuration = sharedDefaults?.double(forKey: "gromo:screentime:totalDuration") ?? 0
+        let lastUpdated = sharedDefaults?.object(forKey: "gromo:screentime:lastUpdated")
+        print("[ScreenTimeModule] getTotalScreenTime - duration: \(totalDuration), lastUpdated: \(lastUpdated ?? "nil")")
         resolve(totalDuration)
     }
 }
