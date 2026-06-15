8e773e1ee4ed99cf27defc98827294a029a92be6
조재영 <whwodud0303@naver.com>
2026-06-15 17:47:01 +0900

    [FEAT] GROMO-277 회원 탈퇴 API 구현

--- back/src/main/java/com/oneorthree/phone/exception/RoomHostCannotWithdrawException.java
+++ back/src/main/java/com/oneorthree/phone/exception/RoomHostCannotWithdrawException.java
@@ -1,7 +1,7 @@
 package com.oneorthree.phone.exception;
 
 public class RoomHostCannotWithdrawException extends RuntimeException {
-  public RoomHostCannotWithdrawException(String message) {
-    super(message);
-  }
+    public RoomHostCannotWithdrawException() {
+        super("방장 위임 후 탈퇴할 수 있습니다.");
+    }
 }
